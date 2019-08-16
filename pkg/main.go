e726b7566b5dfa50750b5e633897e4a9dc2469a8
boris <boris.lenzinger@gmail.com>
2019-08-16 06:37:18 +0200

    Reordering the code.
    The code has been reorganized so it can be read in a linear manner.
    The confirm function has been fixed.

--- pkg/main.go
+++ pkg/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -18,95 +19,31 @@ var stdinMplayer io.WriteCloser
 var currentReadVideo string
 var toDelete map[string]string
 
-func playVideo(pathToVideo string, waitGroup *sync.WaitGroup) {
-	waitGroup.Add(1)
-	mplayer = exec.Command("/usr/bin/mplayer", pathToVideo)
-	mplayer.Start()
-
-	waitGroup.Done()
-}
-
-func stopPlayVideo() {
-	mplayer.Process.Kill()
-}
-
-func getPrev(list []string, currentIndex int) string {
-	if currentIndex > 0 && len(list) > currentIndex {
-		return list[currentIndex-1]
-	}
-	return ""
-}
-
-func getNext(list []string, currentIndex int) string {
-	if currentIndex < len(list)-2 {
-		return list[currentIndex+1]
-	}
-	return ""
-}
-
-func showDetailsOfDeletion(playlist []string, toDelete map[string]string) {
-	totalSize := int64(0)
-
-	var f *os.File
-	var fi os.FileInfo
-	for _, path := range playlist {
-		if _, ok := toDelete[path]; ok {
-			f, _ = os.Open(path)
-			fi, _ = f.Stat()
-			s := fi.Size()
-			fmt.Printf(" * %s %s \n", sizeToMegaOrGiga(s), path)
-			totalSize += s
-		}
-	}
-	if totalSize > 0 {
-		fmt.Printf("                 Total Size: %s \n", sizeToMegaOrGiga(totalSize))
-	}
-}
-
-func sizeToMegaOrGiga(size int64) string {
-	sizeInMega := float64(size) / 1024 / 1024
-	sizeInGiga := float64(sizeInMega) / 1024
-
-	if sizeInGiga < 1 {
-		return fmt.Sprintf("%d Mb", int64(sizeInMega))
-	}
-	return fmt.Sprintf("%.3f Gb", sizeInGiga)
-}
-
 func main() {
 	toDelete := make(map[string]string)
 	playlist := os.Args[1]
-	f, err := os.Open(playlist)
+	videosList, err := getContent(playlist)
 	if err != nil {
-		log.Printf("Error: failed to open playlist %q due to %s", playlist, err)
-		os.Exit(1)
-	}
-	videosListAsBytes, err := ioutil.ReadAll(f)
-	if err != nil {
-		log.Printf("Error: failed to read playlist %q due to %s", playlist, err)
 		os.Exit(1)
 	}
 
 	err = keyboard.Open()
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to capture stdin: %v\n", err)
 	}
 	defer keyboard.Close()
 
-	videosListAsString := string(videosListAsBytes)
-	videosList := strings.Split(videosListAsString, "\n")
 	indexInPlaylist := 0
 	var currentVideo, nextVideo, prevVideo string
+	var waitgroup sync.WaitGroup
 
 	for {
-		log.Println(" ================================================== ")
-		log.Printf("         Video %d/%d\n", indexInPlaylist+1, len(videosList))
-		log.Println(" ================================================== ")
 		currentVideo = videosList[indexInPlaylist]
 		prevVideo = getPrev(videosList, indexInPlaylist)
 		nextVideo = getNext(videosList, indexInPlaylist)
-		var waitgroup sync.WaitGroup
-		log.Printf("Reading video %q\n", currentVideo)
+		log.Println(" ================================================== ")
+		log.Printf("         Video %d/%d\n", indexInPlaylist+1, len(videosList))
+		log.Println(" ================================================== ")
 		if _, ok := toDelete[currentVideo]; ok {
 			fmt.Println()
 			log.Println("       [WARNING] This video has been marked to be deleted")
@@ -121,7 +58,7 @@ func main() {
 		log.Println("  * u : removes video from list to be deleted")
 		log.Println("  * q : quit process. Videos marked as deleted will be proposed for deletion")
 		log.Println()
-		log.Printf("Number of videos marked as to be deleted: %q\n", len(toDelete))
+		log.Printf("Number of videos marked as to be deleted: %d\n", len(toDelete))
 
 		var s string
 		for {
@@ -174,5 +111,116 @@ func main() {
 	}
 endReading:
 
+	keyboard.Close()
+
 	showDetailsOfDeletion(videosList, toDelete)
+	if len(toDelete) > 0 {
+		if !confirm() {
+			log.Println("Deletion was cancelled")
+			return
+		}
+		deleteSelection(toDelete)
+	}
+}
+
+func getContent(fileContainingList string) ([]string, error) {
+	f, err := os.Open(fileContainingList)
+	if err != nil {
+		log.Printf("Error: failed to open playlist %q due to %s", fileContainingList, err)
+		return []string{}, err
+	}
+	videosListAsBytes, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Printf("Error: failed to read playlist %q due to %s", fileContainingList, err)
+		return []string{}, err
+	}
+	videosListAsString := string(videosListAsBytes)
+	return strings.Split(videosListAsString, "\n"), nil
+}
+
+func getPrev(list []string, currentIndex int) string {
+	if currentIndex > 0 && len(list) > currentIndex {
+		return list[currentIndex-1]
+	}
+	return ""
+}
+
+func getNext(list []string, currentIndex int) string {
+	if currentIndex < len(list)-2 {
+		return list[currentIndex+1]
+	}
+	return ""
+}
+
+func playVideo(pathToVideo string, waitGroup *sync.WaitGroup) {
+	waitGroup.Add(1)
+	mplayer = exec.Command("/usr/bin/mplayer", pathToVideo)
+	mplayer.Start()
+
+	waitGroup.Done()
+}
+
+func stopPlayVideo() {
+	mplayer.Process.Kill()
+}
+
+func sizeToMegaOrGiga(size int64) string {
+	sizeInMega := float64(size) / 1024 / 1024
+	sizeInGiga := float64(sizeInMega) / 1024
+
+	if sizeInGiga < 1 {
+		return fmt.Sprintf("%d Mb", int64(sizeInMega))
+	}
+	return fmt.Sprintf("%.3f Gb", sizeInGiga)
+}
+
+func showDetailsOfDeletion(playlist []string, toDelete map[string]string) {
+
+	if len(toDelete) == 0 {
+		log.Println("No file to delete")
+		return
+	}
+	totalSize := int64(0)
+
+	var f *os.File
+	var fi os.FileInfo
+	for _, path := range playlist {
+		if _, ok := toDelete[path]; ok {
+			f, _ = os.Open(path)
+			fi, _ = f.Stat()
+			s := fi.Size()
+			fmt.Printf(" * %s %s \n", sizeToMegaOrGiga(s), path)
+			totalSize += s
+		}
+	}
+	if totalSize > 0 {
+		fmt.Printf("                 Total Size: %s \n", sizeToMegaOrGiga(totalSize))
+	}
+}
+
+func confirm() bool {
+	fmt.Println()
+	fmt.Println("Do you confirm deletion ? (Y/N)")
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		answer, _ := reader.ReadString('\n')
+		answer = strings.ReplaceAll(answer, "\n", "")
+		switch answer {
+		case "y", "Y":
+			return true
+		case "n", "N":
+			return false
+		default:
+			log.Printf("%q is not a supported answer. Please use Y or N\n", answer)
+		}
+	}
+}
+
+func deleteSelection(selection map[string]string) {
+	i := 0
+	for _, path := range selection {
+		log.Printf("(%d/%d) Deleting %q ...\n", i+1, len(selection), path)
+		i++
+	}
 }
